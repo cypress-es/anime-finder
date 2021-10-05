2eddf83e3ea6890a35fef6bfc5cde6a931cdc11d
kevinccbsg <kevinccbsg@gmail.com>
2021-10-05 21:22:52 +0200

    fix: add styles to the search form

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Select, { SingleValue, MultiValue } from 'react-select';
 import { STATUS_OPTIONS, FORMAT_OPTIONS, SEASON_PERIOD } from './data';
 import * as api from '../../repository/api';
+import filterIcon from '../../assets/icons/filter_alt_white_24dp.svg';
+import filterIconOff from '../../assets/icons/filter_alt_off_white_24dp.svg';
 import { SearchFormProps, SelectOptions } from './SearchForm.model';
 import style from './SearchForm.module.scss';
 
+// TODO: refactor in multiple components
 const SearchForm: React.FC<SearchFormProps> = ({ onSubmit }) => {
   const [inputValue, setValue] = useState('');
+  const [showFilters, toggleFilter] = useState<boolean>(false);
   const [genresOptions, setOptions] = useState<SelectOptions[]>([]);
   const [selectedOptions, setSelectedOptions] = useState({});
   useEffect(() => {
@@ -50,63 +54,97 @@ const SearchForm: React.FC<SearchFormProps> = ({ onSubmit }) => {
         <div>
           <label htmlFor="searchAnime">Search yor anime</label>
           <input
-            className="u-full-width"
+            className={`u-full-width ${style.titleInput}`}
             data-cy="search-form-input"
             type="text"
             id="searchAnime"
+            placeholder="search your favorite anime"
             value={inputValue}
             onChange={(e: React.FormEvent<HTMLInputElement>): void => {
               setValue(e.currentTarget.value)
             }}
           />
         </div>
-        <div>
-          <label htmlFor="searchAnime">Status</label>
-          <Select
-            name="status"
-            options={STATUS_OPTIONS}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            onChange={changeSelect('status')}
-          />
-        </div>
-        <div>
-          <label htmlFor="searchAnime">Format</label>
-          <Select
-            name="format"
-            options={FORMAT_OPTIONS}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            onChange={changeSelect('format')}
-          />
-        </div>
-        <div>
-          <label htmlFor="searchAnime">Format</label>
-          <Select
-            name="season_period"
-            options={SEASON_PERIOD}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            onChange={changeSelect('season_period')}
-          />
-        </div>
-        <div>
-          <label htmlFor="searchAnime">Genre</label>
-          <Select
-            isMulti
-            name="genres"
-            options={genresOptions}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            onChange={changeMultipleSelect('genres')}
-          />
-        </div>
-        <div>
+        {showFilters && (
+          <div className={style.filterContainer}>
+            <div>
+              <label htmlFor="searchAnime">Status</label>
+              <Select
+                name="status"
+                options={STATUS_OPTIONS}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                styles={{
+                  placeholder: styles => ({
+                    ...styles,
+                    textAlign: 'left',
+                  }),
+                }}
+                onChange={changeSelect('status')}
+              />
+            </div>
+            <div>
+              <label htmlFor="searchAnime">Format</label>
+              <Select
+                name="format"
+                options={FORMAT_OPTIONS}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                styles={{
+                  placeholder: styles => ({
+                    ...styles,
+                    textAlign: 'left',
+                  }),
+                }}
+                onChange={changeSelect('format')}
+              />
+            </div>
+            <div>
+              <label htmlFor="searchAnime">Format</label>
+              <Select
+                name="season_period"
+                options={SEASON_PERIOD}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                styles={{
+                  placeholder: styles => ({
+                    ...styles,
+                    textAlign: 'left',
+                  }),
+                }}
+                onChange={changeSelect('season_period')}
+              />
+            </div>
+            <div>
+              <label htmlFor="searchAnime">Genre</label>
+              <Select
+                isMulti
+                name="genres"
+                options={genresOptions}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={changeMultipleSelect('genres')}
+              />
+            </div>
+          </div>
+        )}
+        <div className={style.actionsContainer}>
           <input
             className="button-primary"
             type="submit"
             value="Submit"
           />
+          <button
+            className={`button-primary ${style.filterButton}`}
+            onClick={() => toggleFilter(!showFilters)}
+          >
+            {!showFilters && (
+              <img src={filterIcon} alt="filter icon on" />
+            )}
+            {showFilters && (
+              <img src={filterIconOff} alt="filter icon off" />
+            )}
+          </button>
         </div>
       </div>
     </form>
--- src/repository/api.ts
+++ src/repository/api.ts
@@ -4,7 +4,6 @@ import { AnimeItem, Filters, Options } from './APImodel'
 const API = 'https://api.aniapi.com';
 
 const queryBuilder = (query: string, type: string, option: Options): string => {
-  console.log(option);
   if (Array.isArray(option[type])) {
     const arrayFilters = option[type] as unknown as string[];
     const filter = arrayFilters.map((filterValue: string) => (
@@ -37,7 +36,6 @@ const getAnimeList = async (filters?: Filters): Promise<AnimeItem[]> => {
       query = queryBuilder(query, 'season_period', queryFilters.options);
       query = queryBuilder(query, 'genres', queryFilters.options);
     }
-    console.log(query);
     const { data } = await axios.get(`${API}/v1/anime?per_page=10${query}`);
     const list = data.data.documents ? data.data.documents : [];
     return list as AnimeItem[];
