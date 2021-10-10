5d5ab6c44449496ed5947c90d9aede67a7131a00
Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>
2021-10-10 16:09:05 +0200

    fix: add missing intercetps (#4)

--- cy-tests/cypress/integration/homepage_search.spec.js
+++ cy-tests/cypress/integration/homepage_search.spec.js
@@ -7,8 +7,14 @@ describe('Homepage search', () => {
     }, {
       fixture: 'list_response',
     }).as('getAnimeList');
+    cy.intercept({
+      url: 'https://api.aniapi.com/v1/resources/1.0/0',
+      method: 'get',
+    }, {
+      fixture: 'list_genres',
+    }).as('getGenres');
     cy.visit('/');
-    cy.wait('@getAnimeList');
+    cy.wait(['@getAnimeList', '@getGenres']);
     cy.intercept({
       url: 'https://api.aniapi.com/v1/anime?per_page=10&title=flcl',
       method: 'get',
