d559d6682431ce1ff9736b1b315d325591ae6362
kevinccbsg <kevinccbsg@gmail.com>
2021-10-19 23:39:28 +0200

    chore: add server features

---
+++ server.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+const express = require('express');
+const { join } = require('path');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+app.use(compression());
+
+app.use(express.static('dist'));
+app.get('/*', (req, res) => {
+  res.sendFile(join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening PORT: ${PORT}`)
+);
